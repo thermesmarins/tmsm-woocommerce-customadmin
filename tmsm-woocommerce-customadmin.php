853928adb9279356f4c5d10bb8288cbe66ba85a3
Nico Mollet <nico.mollet@gmail.com>
2017-04-28 14:28:42 +0200

    Plugin init with boilerplate

---
+++ tmsm-woocommerce-customadmin.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://github.com/nicomollet
+ * @since             1.0.0
+ * @package           Tmsm_Woocommerce_Customadmin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       TMSM WooCommerce Custom Admin
+ * Plugin URI:        https://github.com/thermesmarins/tmsm-woocommerce-customadmin
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Nicolas Mollet
+ * Author URI:        https://github.com/nicomollet
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       tmsm-woocommerce-customadmin
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-tmsm-woocommerce-customadmin-activator.php
+ */
+function activate_tmsm_woocommerce_customadmin() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-tmsm-woocommerce-customadmin-activator.php';
+	Tmsm_Woocommerce_Customadmin_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-tmsm-woocommerce-customadmin-deactivator.php
+ */
+function deactivate_tmsm_woocommerce_customadmin() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-tmsm-woocommerce-customadmin-deactivator.php';
+	Tmsm_Woocommerce_Customadmin_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_tmsm_woocommerce_customadmin' );
+register_deactivation_hook( __FILE__, 'deactivate_tmsm_woocommerce_customadmin' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-tmsm-woocommerce-customadmin.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_tmsm_woocommerce_customadmin() {
+
+	$plugin = new Tmsm_Woocommerce_Customadmin();
+	$plugin->run();
+
+}
+run_tmsm_woocommerce_customadmin();
